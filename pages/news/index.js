2df9fdea942e752b6bde7b29109d5854d3a987d5
Yeliazar <erik.ruzhakov@mail.ru>
2022-10-06 00:35:13 +0300

    add Linking between Pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,24 @@
-import React from "react";
+import {Fragment} from "react";
+import Link from "next/link";
 
 function NewsPage() {
-    return <h1>The News Page</h1>
+    return (
+        <Fragment>
+            <h1>The News Page</h1>
+            <ul>
+                <li>
+                    <Link href="/news/nextjs-is-good">
+                        NextJS is GOOD
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/news/something-else">
+                        Something else
+                    </Link>
+                </li>
+            </ul>
+        </Fragment>
+    );
 }
 
 export default NewsPage;
