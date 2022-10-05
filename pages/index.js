15861cc81d55c7f6aaf443180f33934f52228e9a
Yeliazar <erik.ruzhakov@mail.ru>
2022-10-06 01:00:28 +0300

    add Components(layout, ui, meetups), updated pages, styles, packages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList";
 
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://media.cntraveler.com/photos/57d87670fd86274a1db91acd/master/pass/most-beautiful-paris-pont-alexandre-iii-GettyImages-574883771.jpg',
+        address: 'Minsk, Oxford street 5',
+        description: 'First meetup!'
+    },
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://cdn.mos.cms.futurecdn.net/pD3bsKPrjsqNiFDGRL5oq6.jpg',
+        address: 'Gomel, Oxford street 10, 456789',
+        description: 'Second meetup!'
+    },
+]
 
 function HomePage() {
-    return <h1>The Home Page</h1>
+    return <MeetupList meetups={DUMMY_MEETUPS} />
 }
 
 export default HomePage;
