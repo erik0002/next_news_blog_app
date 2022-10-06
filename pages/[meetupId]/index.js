939c74bec4a7fb36825532f80de7e505771795d8
Yeliazar <erik.ruzhakov@mail.ru>
2022-10-06 18:10:41 +0300

    fix fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -27,7 +27,7 @@ export async function getStaticPaths() {
     console.log(meetups)
 
     return {
-        fallback: false,
+        fallback: 'blocking',
         paths: meetups.map((meetup) => ({
             params: { meetupId: meetup._id.toString() },
         })),
