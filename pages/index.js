49fd0f9d0eab71447288460383411079537c14a6
Yeliazar <erik.ruzhakov@mail.ru>
2022-10-06 13:46:27 +0300

    add Layout wrapper

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,22 @@
 import MeetupList from "../components/meetups/MeetupList";
+import Layout from "../components/layout/Layout";
 
-const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        title: 'A First Meetup',
-        image: 'https://media.cntraveler.com/photos/57d87670fd86274a1db91acd/master/pass/most-beautiful-paris-pont-alexandre-iii-GettyImages-574883771.jpg',
-        address: 'Minsk, Oxford street 5',
-        description: 'First meetup!'
-    },
-    {
-        id: 'm1',
-        title: 'A First Meetup',
-        image: 'https://cdn.mos.cms.futurecdn.net/pD3bsKPrjsqNiFDGRL5oq6.jpg',
-        address: 'Gomel, Oxford street 10, 456789',
-        description: 'Second meetup!'
-    },
-]
+const DUMMY_MEETUPS = [{
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://media.cntraveler.com/photos/57d87670fd86274a1db91acd/master/pass/most-beautiful-paris-pont-alexandre-iii-GettyImages-574883771.jpg',
+    address: 'Minsk, Oxford street 5',
+    description: 'First meetup!'
+}, {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://cdn.mos.cms.futurecdn.net/pD3bsKPrjsqNiFDGRL5oq6.jpg',
+    address: 'Gomel, Oxford street 10, 456789',
+    description: 'Second meetup!'
+},]
 
 function HomePage() {
-    return <MeetupList meetups={DUMMY_MEETUPS} />
+    return <MeetupList meetups={DUMMY_MEETUPS}/>;
 }
 
 export default HomePage;
